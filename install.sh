26350382f84024be0469bd671b0ec7d5be102997
Owl <owl_proxy@openai.com.do>
2024-06-07 23:52:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# 定义颜色变量
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# 检查操作系统类型
+os_type=$(grep -Ei 'centos|red hat|fedora' /etc/*release)
+
+# 根据操作系统类型选择下载URL
+if [[ -n "$os_type" ]]; then
+    echo -e "${GREEN}检测到CentOS系统，正在获取最新的Owl脚本...${NC}"
+    script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl.sh"
+else
+    echo -e "${GREEN}检测到Debian系统，正在获取最新的Owl脚本...${NC}"
+    script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl_debian.sh"
+fi
+
 # 强制获取最新的脚本
-echo -e "${GREEN}获取最新的Owl脚本...${NC}"
 if [ -f /usr/local/bin/owl ]; then
     sudo rm /usr/local/bin/owl
 fi
-sudo curl -o /usr/local/bin/owl https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl.sh
-
-
+sudo curl -o /usr/local/bin/owl $script_url
 
 # 检查下载是否成功
 if [ $? -ne 0 ]; then
