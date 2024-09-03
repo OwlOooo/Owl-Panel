710b180680a3d8feb5da89afa5ce537f008edb13
Owl <owl_proxy@openai.com.do>
2024-09-03 11:29:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,33 +5,9 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
+script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl.sh"
 
-# 检查操作系统类型
-if grep -Eqi "debian|ubuntu" /etc/os-release; then
-    os_type="debian"
-elif grep -Eqi "centos|red hat|fedora" /etc/os-release; then
-    os_type="centos"
-else
-    os_type="unknown"
-fi
 
-# 调试信息
-echo "${YELLOW}Debug: Detected OS type is $os_type${NC}"
-
-# 根据操作系统类型选择下载URL
-if [ "$os_type" = "centos" ]; then
-    echo "${GREEN}检测到CentOS系统，正在获取最新的Owl脚本...${NC}"
-    script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl.sh"
-elif [ "$os_type" = "debian" ]; then
-    echo "${GREEN}检测到Debian系统，正在获取最新的Owl脚本...${NC}"
-    script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl_debian.sh"
-else
-    echo "${RED}不支持的操作系统。${NC}"
-    exit 1
-fi
-
-# 显示选择的URL
-echo "${YELLOW}Debug: Selected script URL is $script_url${NC}"
 
 # 强制获取最新的脚本
 if [ -f /usr/local/bin/owl ]; then
