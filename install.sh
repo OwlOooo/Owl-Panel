7f958dc4fa8339afd4e91f8ef861fb51709ee908
Owl <owl_proxy@openai.com.do>
2024-09-03 10:58:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,24 +7,27 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 # 检查操作系统类型
-os_type=$(grep -Ei 'centos|red hat|fedora' /etc/*release)
-
-# 检查是否为 ARM 架构
-architecture=$(uname -m)
+if grep -Eqi "debian|ubuntu" /etc/os-release; then
+    os_type="debian"
+elif grep -Eqi "centos|red hat|fedora" /etc/os-release; then
+    os_type="centos"
+else
+    os_type="unknown"
+fi
 
 # 调试信息
-echo "${YELLOW}Debug: Architecture is $architecture${NC}"
+echo "${YELLOW}Debug: Detected OS type is $os_type${NC}"
 
-# 根据操作系统类型和架构选择下载URL
-if [ -n "$os_type" ]; then
+# 根据操作系统类型选择下载URL
+if [ "$os_type" = "centos" ]; then
     echo "${GREEN}检测到CentOS系统，正在获取最新的Owl脚本...${NC}"
     script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl.sh"
-elif echo "$architecture" | grep -Eq 'arm|aarch64'; then
-    echo "${GREEN}检测到Debian ARM系统，正在获取最新的Owl ARM脚本...${NC}"
-    script_url="https://raw.githubusercontent.com/OwlOooo/Owl-Panel/main/owl_debian_arm.sh"
-else
+elif [ "$os_type" = "debian" ]; then
     echo "${GREEN}检测到Debian系统，正在获取最新的Owl脚本...${NC}"
     script_url="https://raw.githubusercontent.com/OwlOooo/owl-panel/main/owl_debian.sh"
+else
+    echo "${RED}不支持的操作系统。${NC}"
+    exit 1
 fi
 
 # 显示选择的URL
@@ -33,14 +36,14 @@ echo "${YELLOW}Debug: Selected script URL is $script_url${NC}"
 # 强制获取最新的脚本
 if [ -f /usr/local/bin/owl ]; then
     echo "${GREEN}旧的脚本存在，删除它...${NC}"
-    sudo rm /usr/local/bin/owl
+    sudo rm -f /usr/local/bin/owl
 fi
 
 # 添加时间戳参数
 timestamp=$(date +%s)
 
 echo "${GREEN}下载最新的脚本...${NC}"
-sudo curl -H 'Cache-Control: no-cache' -o /usr/local/bin/owl -L "${script_url}?t=${timestamp}"
+sudo curl -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" -H "Expires: 0" -o /usr/local/bin/owl -L "${script_url}?t=${timestamp}"
 
 # 检查下载是否成功
 if [ $? -ne 0 ]; then
